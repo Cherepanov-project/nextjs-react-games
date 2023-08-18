45a19fcb4d7e0e33d6121473b144e1d3aef77e9a
Pavel Poryvai <42907367+Pavel377dq@users.noreply.github.com>
2023-08-18 23:25:35 +0300

    aim trainer fix wrong fix with redux-persist failed to create sync storage and returning to noop storage

--- aim-trainer/reducers/root-reducer.ts
+++ aim-trainer/reducers/root-reducer.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { combineReducers } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 // import storage from 'redux-persist/lib/storage';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
 
 import { boardReducer } from './board-slice';
 import { targetsReducer } from './targets-slice';
@@ -10,10 +11,23 @@ import { statisticsReducer } from './statistics-slice';
 import { difficultReducer } from './difficulty-slice';
 import { leaderListReducer } from './leader-list-slice';
 
+const createNoopStorage = () => ({
+  getItem(_key: any) {
+    return Promise.resolve(null);
+  },
+  setItem(_key: any, value: any) {
+    return Promise.resolve(value);
+  },
+  removeItem(_key: any) {
+    return Promise.resolve();
+  },
+});
+const storage = typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage();
+
 export const aimTrainerRootReducer = persistReducer(
   {
     key: 'aim-trainer',
-    storage: AsyncStorage,
+    storage,
     whitelist: ['leaderList'],
   },
   combineReducers({
